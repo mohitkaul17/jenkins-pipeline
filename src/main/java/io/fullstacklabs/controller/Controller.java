ba87835d3ca4a5f08310519bff167fcc112ed203
Mohit Kaul <kaulmohit17@gmail.com>
2021-04-04 19:02:22 -0400

    feat(project):added controller for testing branch 2 fixed controller

--- src/main/java/io/fullstacklabs/controller/Controller.java
+++ src/main/java/io/fullstacklabs/controller/Controller.java
@@ -1,14 +1,13 @@
 package io.fullstacklabs.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @RequestMapping("branches/{id}")
-    public String getTestString(@PathVariable int id) {
-        return "You are currently on branch #: " + id;
+    @RequestMapping("branches/second")
+    public String getTestString() {
+        return "You are currently on branch second: ";
     }
 }
